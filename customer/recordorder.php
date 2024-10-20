69300fa5ecfd500fb33b072e44fc679557d572f5
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 05:47:13 +0700

    m

--- customer/recordorder.php
+++ customer/recordorder.php
@@ -7,15 +7,16 @@
 			$sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid] ;
 			@$total += $sum[$pid] ;
 		}
-	
-	$sql = "insert into `orders` values('', '$total', CURRENT_TIMESTAMP, '0');" ;
-	mysqli_query($conn, $sql) or die ("insert error") ;
-	$id = mysqli_insert_id($conn);
-	
-	foreach($_SESSION['sid'] as $pid) {
-		$sql2 = "insert into orders_detail values('', '$id', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."');" ;
-		mysqli_query($conn, $sql2);
-	}
-	echo $product['p_id'];	
-echo "<meta http-equiv=\"refresh\" content=\"0;URL=cosclear.php\">";
+		$sql = "INSERT INTO orders (ototal, odate, status) VALUES ('$total', CURRENT_TIMESTAMP, '0')";
+		if (mysqli_query($conn, $sql)) {
+			$id = mysqli_insert_id($conn);
+			foreach ($_SESSION['sid'] as $pid) {
+				$sql2 = "INSERT INTO orders_detail (od_id, p_id, quantity) VALUES ('$id', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."')";
+				mysqli_query($conn, $sql2);
+			}
+			echo "<meta http-equiv='refresh' content='0;URL=cosclear.php'>";
+		} else {
+			echo "Error: " . mysqli_error($conn);
+		}
+		
 ?>
