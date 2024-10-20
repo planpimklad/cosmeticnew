883af9668ba69254b570085dbbbf4ff08f5f04d1
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 05:54:05 +0700

    c

--- customer/recordorder.php
+++ customer/recordorder.php
@@ -2,21 +2,25 @@
 <?php
 	@session_start();
 	include("connectdb.php");
-	
-		foreach($_SESSION['sid'] as $pid) {
-			$sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid] ;
-			@$total += $sum[$pid] ;
-		}
-		$sql = "INSERT INTO orders (ototal, odate, status) VALUES ('$total', CURRENT_TIMESTAMP, '0')";
-		if (mysqli_query($conn, $sql)) {
-			$id = mysqli_insert_id($conn);
-			foreach ($_SESSION['sid'] as $pid) {
-				$sql2 = "INSERT INTO orders_detail (od_id, p_id, quantity) VALUES ('$id', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."')";
-				mysqli_query($conn, $sql2);
-			}
-			echo "<meta http-equiv='refresh' content='0;URL=cosclear.php'>";
-		} else {
-			echo "Error: " . mysqli_error($conn);
-		}
-		
+	$sql = "SELECT * FROM orders WHERE oid='{$_GET['id']}'";
+$rs = mysqli_query($conn, $sql);
+$data = mysqli_fetch_array($rs);
+$id = $_GET['id'];
+foreach($_SESSION['sid'] as $pid) {
+	$sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid] ;
+	@$total += $sum[$pid] ;
+}
+
+$sql = "insert into `orders` values('', '$total', CURRENT_TIMESTAMP, '0');" ;
+mysqli_query($conn, $sql) or die ("insert error") ;
+$id = mysqli_insert_id($conn);
+
+foreach($_SESSION['sid'] as $pid) {
+$sql2 = "insert into orders_detail values('', '$id', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."');" ;
+mysqli_query($conn, $sql2);
+}
+@session_start();
+session_destroy();	
+echo "สั่งซื้อสินค้าเรียบร้อย";
+echo "<meta http-equiv=\"refresh\" content=\"0;URL=cosclear.php\">";
 ?>
