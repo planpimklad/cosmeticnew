23ae6cb8d3fb48c7e8e6564a71721645b4670595
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 03:52:21 +0700

    a

--- admin/connectdb.php
+++ admin/connectdb.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost" ;
 	$usr = "root";
-	$pwd = "";
+	$pwd = "1478963";
 	$db = "cosmeticnew";
 
 	$conn = mysqli_connect($host,$usr,$pwd) or die("เชื่อมต่อฐานข้อมูลไม่ได้");
