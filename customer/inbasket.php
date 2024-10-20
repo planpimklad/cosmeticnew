bb41b340ad88d531aa3b9ca1795c3cc7190afae9
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 05:08:27 +0700

    c

--- customer/inbasket.php
+++ customer/inbasket.php
@@ -1,143 +1,127 @@
-<?php
-// Enable error reporting for development purposes (remove in production)
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-session_start();  // Start the session
-
-// Include the database connection file
+ <?php
+error_reporting(E_NOTICE);
+@session_start();
 include_once("connectdb.php");
-
-// Retrieve product details based on the ID passed via GET
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    
-    // Query to fetch the product details
-    $sql = "SELECT * FROM product WHERE p_id='$id'";
-    $rs = mysqli_query($conn, $sql);
-    
-    if ($rs) {
-        $data = mysqli_fetch_array($rs);
-        
-        // Store product data in session
-        $_SESSION['sid'][$id] = $data['p_id'];
-        $_SESSION['sname'][$id] = $data['p_name'];
-        $_SESSION['sprice'][$id] = $data['p_price'];
-        $_SESSION['sdetail'][$id] = $data['p_detail'];
-        $_SESSION['spicture'][$id] = $data['p_picture'];
-        @$_SESSION['sitem'][$id]++;
-    } else {
-        echo "Error fetching product: " . mysqli_error($conn);
-    }
+@$kw = $_POST['search'];
+$sql ="select * from product where p_id='{$_GET['id']}' ";
+$rs = mysqli_query($conn, $sql) ;
+$data = mysqli_fetch_array($rs);
+$id = $_GET['id'] ;
+if(isset($_GET['id'])) {
+$_SESSION['sid'][$id] = $data['p_id'];
+$_SESSION['sname'][$id] = $data['p_name'];
+$_SESSION['sprice'][$id] = $data['p_price'];
+$_SESSION['sdetail'][$id] = $data['p_detail'];
+$_SESSION['spicture'][$id] = $data['p_picture'];
+@$_SESSION['sitem'][$id]++;
 }
 ?>
-
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="utf-8">
-    <title>สินค้าในรถเข็น</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.css" />
-    <style>
-        body {
-            background-image: url('Home.jpg');
-            background-repeat: no-repeat;
-            background-size: cover;
-            height: 100%;
-            margin: 0;
-        }
-        body, td, th {
-            font-size: medium;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-    </style>
+<meta charset="utf-8">
+<title>สินค้าในรถเข็น</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="bootstrap.css" rel="stylesheet" type="text/css">
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.7/css/dataTables.dataTables.css" />
+ 
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ 
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+ <style type="text/css">
+ body {
+background-image: url(Home%20.jpg);
+height: 100%;
+margin: 0px;
+background-repeat: no-repeat;
+background-size: cover;
+}
+ body,td,th {
+font-size: medium;
+font-family: Arial, Helvetica, sans-serif;
+}
+ </style>
 </head>
+
 <body>
-
-<div class="container mt-5">
-    <div class="p-3 text-center text-white" style="background-color: #ec407a;">
-        <h2>สินค้าในรถเข็น</h2>
-    </div>
-    <br>
-    <center>
-        <a href="type.php" class="btn btn-primary">กลับไปเลือกสินค้า</a>
-
-        <?php if (empty($_SESSION['sid'])) { ?>
-            <a href="#" class="btn btn-danger" onClick="alert('ไม่มีสินค้าในตะกร้า');">ลบสินค้าทั้งหมด</a>
-        <?php } else { ?>
-            <a href="cosclear.php" class="btn btn-danger" onClick="return confirm('ยืนยันการลบ?');">ลบสินค้าทั้งหมด</a>
-        <?php } ?>
-
-        <?php if (empty($_SESSION['sid'])) { ?>
-            <a href="#" class="btn btn-success" onClick="alert('กรุณาเลือกสินค้า');">สั่งซื้อสินค้า</a>
-        <?php } else { ?>
-            <a href="recordorder.php?id=<?= $id ?>" class="btn btn-success" onClick="return confirm('ยืนยันการสั่ง?');">สั่งซื้อสินค้า</a>
-        <?php } ?>
-    </center>
-    <br><br>
-
-    <div class="container border">
-        <table id="myTable" class="table table-striped table-dark table-hover">
-            <thead>
-                <tr>
-                    <th>รูปสินค้า</th>
-                    <th>ชื่อสินค้า</th>
-                    <th>ราคา/ชิ้น</th>
-                    <th>จำนวน(ชิ้น)</th>
-                    <th>รวม</th>
-                    <th>ลบ</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                if (!empty($_SESSION['sid'])) {
-                    $total = 0;
-                    foreach ($_SESSION['sid'] as $pid) {
-                        // Query to fetch product data from the database
-                        $sql_product = "SELECT * FROM product WHERE p_id='$pid'";
-                        $rs_product = mysqli_query($conn, $sql_product);
-                        $data_product = mysqli_fetch_array($rs_product);
-
-                        if ($data_product) {
-                            $img = "images/" . $data_product['p_picture'];
-                            $sum = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid];
-                            $total += $sum;
-                ?>
-                            <tr>
-                                <td><img src="<?= $img; ?>" width="200"></td>
-                                <td><?= $_SESSION['sname'][$pid]; ?></td>
-                                <td><?= number_format($_SESSION['sprice'][$pid], 0); ?></td>
-                                <td><?= $_SESSION['sitem'][$pid]; ?></td>
-                                <td><?= number_format($sum, 0); ?></td>
-                                <td><a href="cosclear2.php?id=<?= $pid; ?>" class="btn btn-danger">ลบ</a></td>
-                            </tr>
-                <?php
-                        }
-                    }
-                ?>
-                    <tr>
-                        <td colspan="4" class="text-right"><strong>รวมทั้งสิ้น</strong></td>
-                        <td><strong><?= number_format($total, 0); ?></strong></td>
-                        <td><strong>บาท</strong></td>
-                    </tr>
-                <?php
-                } else {
-                ?>
-                    <tr>
-                        <td colspan="6" class="text-center">ไม่มีสินค้าในตะกร้า</td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
+ <div class="full-background">
 </div>
+<blockquote>
+<div class="p-3 text-primary-emphasis border border-primary-subtle rounded-3" style="background-color: #ec407a"
+ align='center' style='width:100%;'><h2 style="color: white;">สินค้าในรถเข็น</h2></div>
+<br>
+<center>
+</style>
+<a href="type.php" class="btn btn-primary" width="200px">กลับไปเลือกสินค้า</a> 
+<?php
+if(empty($_SESSION['sid'])) {
+?>
+<a href="#" class="btn btn-sm btn-danger" width="200px" onClick="alert('ไม่มีสินค้าในตะกร้า');">ลบสินค้าทั้งหมด</a>
+<?php } else { ?>
+<a href="cosclear.php?pid" onClick="return confirm('ยืนยันการลบ?');" type="button"class="btn btn-sm btn-danger">ลบสินค้าทั้งหมด</a>
+<?php } ?>
+<?php
+if(empty($_SESSION['sid'])) {
+?>
+<a href="" class="btn btn-success" onClick="alert('กรุณาเลือกสินค้า');">สั่งซื้อสินค้า</a>
+<?php } else { ?>
+<a href="recordorder.php?id=<?= $product['p_id']; ?>" class="btn btn-success" onClick="return confirm('ยืนยันการสั่ง?');" 
+ >สั่งซื้อสินค้า</a>	
+<?php } ?><br><br></center>
+<div class="container border">
+<table id="myTable" class="table table-striped table-dark table-hover" style="width:100%">
+  <thead>
+    <tr>
+      <th>รูปสินค้า</th>
+      <th>ชื่อสินค้า</th>
+      <th>ราคา/ชิ้น</th>
+      <th>จำนวน(ชิ้น)</th>
+      <th>รวม</th>
+      <th ></th>
+      <th></th>
+    </tr>
+  </thead>
+ <tbody>
+    <?php
+    if (!empty($_SESSION['sid'])) {
+        foreach ($_SESSION['sid'] as $pid) {
+            // ดึงข้อมูลสินค้าแต่ละตัวจากฐานข้อมูล
+            $sql_product = "SELECT * FROM product WHERE p_id='$pid'";
+            $rs_product = mysqli_query($conn, $sql_product);
+            $data_product = mysqli_fetch_array($rs_product);
 
-<script src="https://cdn.datatables.net/2.1.7/js/jquery.dataTables.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-    });
-</script>
-</body>
-</html>
+            // ตรวจสอบว่ามีข้อมูลสินค้าหรือไม่
+            if ($data_product) {
+                $img = "images/" . $data_product['p_id'] . "." . $data_product['p_picture'];
+                @$i++;
+                $sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid];
+                @$total += $sum[$pid];
+    ?>
+                <tr>
+                    <td><img src="<?= $img; ?>" width="200"></td>
+                    <td><?= $_SESSION['sname'][$pid]; ?></td>
+                    <td><?= number_format($_SESSION['sprice'][$pid], 0); ?></td>
+                    <td><?= $_SESSION['sitem'][$pid]; ?></td>
+                    <td><?= number_format($sum[$pid], 0); ?></td>
+                    <td><a href="cosclear2.php?id=<?= $pid; ?>" class="btn btn-danger">ลบ</a></td>
+                    <td></td>
+                </tr>
+    <?php
+            }
+        } // end foreach
+    ?>
+            <tr>
+                <td colspan="5" align="right"><strong>รวมทั้งสิ้น</strong> &nbsp; </td>
+                <td><strong><?= number_format($total, 0); ?></strong></td>
+                <td><strong>บาท</strong></td>
+            </tr>
+    <?php
+    } else {
+    ?>
+            <tr>
+                <td colspan="7" height="50" align="center">ไม่มีสินค้าในตะกร้า</td>
+            </tr>
+    <?php } // end if ?>
+</tbody>
--- customer/recordorder.php
+++ customer/recordorder.php
@@ -1,49 +1,21 @@
+<meta charset="utf-8">
 <?php
-session_start();  // Start session
-include("connectdb.php");  // Include database connection
-
-// Initialize total sum variable
-$total = 0;
-
-// Calculate the total price for the order
-if (isset($_SESSION['sid'])) {
-    foreach ($_SESSION['sid'] as $index => $pid) {
-        // Calculate sum per product
-        $sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid];
-        // Accumulate total
-        $total += $sum[$pid];
-    }
-    
-    // Insert into `orders` table
-    $sql = "INSERT INTO `orders` (`oid`, `ototal`, `odate`, `status`) VALUES (NULL, '$total', CURRENT_TIMESTAMP, '0');";
-    
-    // Check if the query was successful
-    if (mysqli_query($conn, $sql)) {
-        // Get the last inserted order ID
-        $order_id = mysqli_insert_id($conn);
-        
-        // Insert order details
-        foreach ($_SESSION['sid'] as $index => $pid) {
-            $item_quantity = $_SESSION['sitem'][$pid];  // Quantity of the item
-            $sql2 = "INSERT INTO `orders_detail` (`id`, `order_id`, `product_id`, `quantity`) VALUES (NULL, '$order_id', '$pid', '$item_quantity');";
-            
-            // Execute query and check if successful
-            if (!mysqli_query($conn, $sql2)) {
-                echo "Error inserting order details: " . mysqli_error($conn);
-            }
-        }
-        
-        // Redirect to cosclear.php after successful order
-        header("Location: cosclear.php");
-        exit();  // Ensure no further code is executed after redirect
-    } else {
-        // Display error if order insertion failed
-        echo "Error inserting order: " . mysqli_error($conn);
-    }
-} else {
-    echo "No items in the cart to process.";
-}
-
-// Close the connection
-mysqli_close($conn);
+	@session_start();
+	include("connectdb.php");
+	
+		foreach($_SESSION['sid'] as $pid) {
+			$sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid] ;
+			@$total += $sum[$pid] ;
+		}
+	
+	$sql = "insert into `orders` values('', '$total', CURRENT_TIMESTAMP, '0');" ;
+	mysqli_query($conn, $sql) or die ("insert error") ;
+	$id = mysqli_insert_id($conn);
+	
+	foreach($_SESSION['sid'] as $pid) {
+		$sql2 = "insert into orders_detail values('', '$id', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."');" ;
+		mysqli_query($conn, $sql2);
+	}
+	
+echo "<meta http-equiv=\"refresh\" content=\"0;URL=cosclear.php\">";
 ?>
