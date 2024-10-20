906424f83b52cb5fdd347f9efa373e4e365b5772
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 05:26:14 +0700

    r

--- customer/inbasket.php
+++ customer/inbasket.php
@@ -67,6 +67,8 @@ if(empty($_SESSION['sid'])) {
 ?>
 <a href="" class="btn btn-success" onClick="alert('กรุณาเลือกสินค้า');">สั่งซื้อสินค้า</a>
 <?php } else { ?>
+<a href="cosview_order.php?id=<?= $product['p_id']; ?>" class="btn btn-success" onClick="return confirm('ยืนยันการสั่ง?');" 
+ >ประวัติการสั่งซื้อ</a>
 <a href="recordorder.php?id=<?= $product['p_id']; ?>" class="btn btn-success" onClick="return confirm('ยืนยันการสั่ง?');" 
  >สั่งซื้อสินค้า</a>	
 <?php } ?><br><br></center>
--- customer/recordorder.php
+++ customer/recordorder.php
@@ -1,29 +1,21 @@
 <meta charset="utf-8">
 <?php
-    // เริ่มเซสชัน
-    @session_start();
-    include("connectdb.php"); // นำเข้าไฟล์เชื่อมต่อฐานข้อมูล
-
-    // คำนวณยอดรวมของสินค้าในตะกร้า
-    foreach($_SESSION['sid'] as $pid) {
-        // คำนวณราคาสินค้าแต่ละชิ้น (ราคาต่อชิ้น * จำนวน)
-        $sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid];
-        @$ototal += $sum[$pid]; // คำนวณยอดรวมทั้งหมด
-    }
-
-    // สร้างคำสั่ง SQL เพื่อบันทึกข้อมูลการสั่งซื้อในตาราง orders
-    $sql = "INSERT INTO `orders` VALUES('', '$ototal', CURRENT_TIMESTAMP, '0');";
-    mysqli_query($conn, $sql) or die ("เกิดข้อผิดพลาดในการบันทึกข้อมูลการสั่งซื้อ"); // ดำเนินการคำสั่ง SQL
-
-    // รับค่า ID ของคำสั่งซื้อที่เพิ่งถูกบันทึก
-    $id = mysqli_insert_id($conn);
-
-    // บันทึกรายละเอียดสินค้าที่สั่งซื้อแต่ละรายการในตาราง orders_detail
-    foreach($_SESSION['sid'] as $pid) {
-        $sql2 = "INSERT INTO orders_detail VALUES('', '$oid', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."');";
-        mysqli_query($conn, $sql2); // ดำเนินการคำสั่ง SQL
-    }
-
-    // เปลี่ยนเส้นทางไปยังหน้าถัดไป (cosclear.php)
-    echo "<meta http-equiv=\"refresh\" content=\"0;URL=cosclear.php\">";
+	@session_start();
+	include("connectdb.php");
+	
+		foreach($_SESSION['sid'] as $pid) {
+			$sum[$pid] = $_SESSION['sprice'][$pid] * $_SESSION['sitem'][$pid] ;
+			@$total += $sum[$pid] ;
+		}
+	
+	$sql = "insert into `orders` values('', '$total', CURRENT_TIMESTAMP, '0');" ;
+	mysqli_query($conn, $sql) or die ("insert error") ;
+	$id = mysqli_insert_id($conn);
+	
+	foreach($_SESSION['sid'] as $pid) {
+		$sql2 = "insert into orders_detail values('', '$id', '".$_SESSION['sid'][$pid]."', '".$_SESSION['sitem'][$pid]."');" ;
+		mysqli_query($conn, $sql2);
+	}
+	
+echo "<meta http-equiv=\"refresh\" content=\"0;URL=cosclear.php\">";
 ?>
