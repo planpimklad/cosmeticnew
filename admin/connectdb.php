1c41f9bda06e8ad358a1ca056ed8d1a2bfd93dee
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 04:56:58 +0700

    a

--- admin/connectdb.php
+++ admin/connectdb.php
@@ -3,7 +3,7 @@
 	$usr = "root";
 	$pwd = "1478963";
 	$db = "cosmeticnew";
-
+ 
 	$conn = mysqli_connect($host,$usr,$pwd) or die("เชื่อมต่อฐานข้อมูลไม่ได้");
 	mysqli_select_db($conn,$db) or die("เลือกฐานข้อมูลไม่ได้");
 	mysqli_query($conn,"SET NAMES utf8");
