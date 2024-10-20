3eac55dce7787b170f97ddfbc63ab02bec58564b
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 04:13:05 +0700

    a

--- customer/connectdb.php
+++ customer/connectdb.php
@@ -2,7 +2,7 @@
     $host = "localhost" ;
 	$usr = "root";
 	$pwd = "1478963";
-	$db = "cosmeticnew";
+	$db = "shop1";
 
 	$conn = mysqli_connect($host,$usr,$pwd) or die("เชื่อมต่อฐานข้อมูลไม่ได้");
 	mysqli_select_db($conn,$db) or die("เลือกฐานข้อมูลไม่ได้");
