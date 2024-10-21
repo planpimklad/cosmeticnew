5724be5e7ebc8d9d26bd4da02df7e2fbe93c0d97
planpimklad <phimphkladpnisa@gmail.com>
2024-10-21 10:48:22 +0700

    d

--- admin/insert.php
+++ admin/insert.php
@@ -109,6 +109,8 @@
            }
 
            // Escape ข้อมูลที่กรอกจากฟอร์มเพื่อป้องกัน SQL injection
+           $pid = mysqli_real_escape_string($conn, $_POST['pid']);
+
            $pname = mysqli_real_escape_string($conn, $_POST['pname']);
            $pdetail = mysqli_real_escape_string($conn, $_POST['pdetail']);
            $pprice = mysqli_real_escape_string($conn, $_POST['pprice']);
@@ -118,7 +120,7 @@
            if (!empty($pname) && !empty($pprice) && !empty($pcat)) {
                // บันทึกข้อมูลสินค้า
                $sql = "INSERT INTO `product` (`p_id`,`p_name`, `p_detail`, `p_price`, `p_picture`, `c_id`) 
-                       VALUES ('{$pname}', '{$pdetail}', '{$pprice}', '{$file_name}', '{$pcat}')";
+                       VALUES ('{$pid}','{$pname}', '{$pdetail}', '{$pprice}', '{$file_name}', '{$pcat}')";
                
                if (!mysqli_query($conn, $sql)) {
                    die('เกิดข้อผิดพลาดในการเพิ่มสินค้า: ' . mysqli_error($conn));
